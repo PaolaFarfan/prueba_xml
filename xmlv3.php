5014f5d96bb5205b246029934f0f99e37f84b219
Maytemoises2020 <astrid.farfan.taipe@gmail.com>
2025-12-12 10:03:41 -0500

    bbb

--- xmlv3.php
+++ xmlv3.php
@@ -15,23 +15,47 @@ foreach($xml as $i_pe => $pe){
     echo 'Codigo: ' .$pe->codigo.'<br>';
     echo 'Tipo: ' .$pe->tipo.'<br>';
     echo 'Nombre: ' .$pe->nombre.'<br>';
-    $consulta_plan = "INSERT INTO sigi_planes_estudio WHERE id_programa_estudios=" . $i_pe; 
+    // Insertar plan en BD
+        $consulta = "INSERT INTO sigi_programa_estudios (codigo, tipo, nombre) 
+                     VALUES ('$pe->codigo', '$pe->tipo', '$pe->nombre')";
+        $conexion->query($consulta);
+        $id_programa_estudios = $conexion->insert_id;
     foreach($pe->planes_estudio[0] as $i_ple => $plan){
         echo '--: '.$plan->nombre.'<br>';
         echo '--: '.$plan->resolucion.'<br>';
         echo '--: '.$plan->fecha_registro.'<br>';
+            // Insertar plan en BD
+        $consulta = "INSERT INTO sigi_planes_estudio (id_programa_estudios, nombre, resolucion, perfil_egresado) 
+                     VALUES ('$id_programa_estudios', '$plan->nombre', '$plan->resolucion', '$plan->perfil_egresado')";
+        $conexion->query($consulta);
+        $id_plan = $conexion->insert_id;
         foreach($plan->modulos_formativos[0] as $id_mod =>$modulo){
             echo '---:'.$modulo->descripcion.'<br>';
             echo '---:'.$modulo->nro_modulo.'<br>';
+            // Insertar plan en BD
+        $consulta = "INSERT INTO sigi_modulo_formativo (id_plan_estudio, descripcion, nro_modulo) 
+             VALUES ('$id_plan', '{$modulo->descripcion}', '{$modulo->nro_modulo}')";
+        $conexion->query($consulta);
+        $id_modulo = $conexion->insert_id;
             foreach($modulo->periodos[0] as $id_per => $periodo){
                 echo '--- :'.$periodo->descripcion.'<br>';
-                foreach($periodo->unidades_didacticas[0] as $id_ud => $ud){
-                    echo '----:'.$ud->nombre.'<br>';
-                    /*echo '----:'.$ud->creditos_teorico.'<br>';
-                    echo '----:'.$ud->creditos_practico.'<br>';
-                    echo '----:'.$ud->tipo.'<br>';
-                    echo '----:'.$ud->horas_semanal.'<br>';
-                    echo '----:'.$ud->horas_semestral.'<br>';*/
+                // Insertar plan en BD
+        $consulta = "INSERT INTO sigi_semestre (id_modulo_formativo, descripcion) 
+                     VALUES ('$id_modulo', '$periodo->descripcion')";
+        $conexion->query($consulta);
+        $id_semestre = $conexion->insert_id;
+                foreach ($periodo->unidades_didacticas[0] as $id_ud => $ud) {
+                    echo '---'. $ud->nombre.'<br>';
+                    echo '---'. $ud->creditos_teorico.'<br>';
+                    echo '---'. $ud->creditos_practico.'<br>';
+                    echo '---'. $ud->tipo.'<br>'; 
+                    echo '---'. $ud->horas_semanal.'<br>'; 
+                    echo '---'. $ud->horas_semestral.'<br>'; 
+                    $cont++;
+                    $consulta = "INSERT INTO sigi_unidad_didactica (id_semestre, nombre, creditos_teorico, creditos_practico, tipo,orden) 
+                        VALUES ('$id_semestre','$ud->nombre','$ud->creditos_teorico','$ud->creditos_practico','$ud->tipo', '$cont')";
+                    $conexion->query($consulta);
+                    $id_ud = $conexion->insert_id;
                 }
             }
         } 
